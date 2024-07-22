af2b2c355e14977f5086d32c2a02f79f964300b9
Илья Пухов <ilapuhov@MacBook-Pro-Ila.local>
2024-07-22 15:14:07 +0300

    Quartz sync: Jul 22, 2024, 3:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🚀 Тяга к Жизни",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
