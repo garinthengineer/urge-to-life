4d04e64412466edd76ad8aefaedb14cb118c4b08
Илья Пухов <ilapuhov@MacBook-Pro-Ila.local>
2024-07-23 09:20:17 +0300

    Quartz sync: Jul 23, 2024, 9:20 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 2, // how many hops of notes to display
+        scale: 1.2, // default view scale
+        showTags: false,
+      }}),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
