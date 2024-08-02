9572f88b63f4abf4cbce5bc6eda5c50e7266d874
Илья Пухов <ilapuhov@MacBook-Pro-Ila.local>
2024-08-02 17:21:34 +0300

    Quartz sync: Aug 2, 2024, 5:21 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
     // Component.DesktopOnly(Component.RecentNotes()),
   ],
   right: [
-    Component.Graph({localGraph:{depth: 3,},globalGraph: {}}),
+    Component.Graph({localGraph:{depth: 3, scale: .6, showTags: false},globalGraph: {}}),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
